4cd962b1be1b3356fdbdd40f520f56ee66310a2f
abdullahwaseem5054-gif <abdullahwaseem5054@gmail.com>
2026-01-22 13:27:02 +0500

    a

--- tests/payment.spec.js
+++ tests/payment.spec.js
@@ -4,6 +4,5 @@ let t1;
 test('Open Stripe Payment Screen', async({page})=>{
     t1= new Payment(page);
     await t1.payment();
-    await t1.fill('4242424242424242','12/28','123','Muhammad Abdullah').use({ browserName: 'chromium' });
-
+    await t1.fill('4242424242424242','12/28','123','Muhammad Abdullah');
 });
