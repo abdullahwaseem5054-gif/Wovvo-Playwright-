1e8ad499fd38b4e6ba0293d50310dd36bd7e697c
abdullahwaseem5054-gif <abdullahwaseem5054@gmail.com>
2026-01-22 12:44:42 +0500

    a

--- pages/editprofile.js
+++ pages/editprofile.js
@@ -1,12 +1,13 @@
 class EditProfile{
     constructor(page){
         this.page=page;
-        this.dropdown=page.locator('//*[@id="radix-_r_0_"]/div/div[2]');
+        this.dropdown=page.locator('//*[@id="radix-_r_g_"]');
         this.editprofilebtn=page.locator('#edit-profile');
         this.firstname= page.locator('#firstName');
         this.lastname=page.locator('#lastName');
-        this.save=page.locator('xpath=/html/body/div[2]/div/div[2]/div[2]/div[2]/div[3]/button');
-
+        this.save=page.locator('xpath=/html/body/div[2]/main/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/button');
+        this.successToast = page.getByText('Profile updated successfully!');
+    
     }
 
     async open2() { 
@@ -14,8 +15,6 @@ class EditProfile{
     }
 
     async edit(firstname,lastname){
-        await this.dropdown.click();
-        await this.editprofilebtn.click();
         await this.firstname.click();
         await this.firstname.clear();
         await this.firstname.fill(firstname);
@@ -23,11 +22,7 @@ class EditProfile{
         await this.lastname.clear();
         await this.lastname.fill(lastname);
         await this.save.click();
-        
-
-
-
-    }
-
+    }  
+    
 }
 module.exports=EditProfile;
--- tests/editprofile.spec.js
+++ tests/editprofile.spec.js
@@ -1,10 +1,10 @@
 import {test,expect} from '@playwright/test';
 import EditProfile from '../pages/editprofile';
 
-
 test.use({ storageState: 'auth.json' });
 test('edit profile', async({page})=>{
     const editprofile1=new EditProfile(page);
     await editprofile1.open2();
-    await editprofile1.edit('Muhammad','Abdullah(QA)');
+    await editprofile1.edit('Muhammad','Abdullah(SQA Engineer)');
+    await expect(page.getByText('Profile updated successfully!')).toBeVisible();
 });
